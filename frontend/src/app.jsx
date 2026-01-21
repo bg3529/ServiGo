4cad96bf31735b3ec1ff18351dad488de0a04583
sudip bayalkoti <sbayalkoti49@gmail.com>
2026-01-21 10:21:15 +0545

    error resulting

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,15 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing/Landing";
-import Profile from "./pages/Profile/Profile";
+import React from "react";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+import LoginForm from "./Components/LoginForm";
+import RegisterForm from "./Components/RegisterForm";
+import "./Components/Auth.css";
+import ForgotPassword from "./Components/forgotpassword";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="app-container">
+      <div className="nav-bar">
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+      </div>
+
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
